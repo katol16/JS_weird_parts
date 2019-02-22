c0aeaafe0e1016c438a5be7526c3a568e09943f5
Karol <karol.vogelgezang@gmail.com>
2019-02-22 21:26:42 +0100

    Sekcja 9

--- Sekcja 8/app.js
+++ Sekcja 8/app.js
@@ -5,8 +5,18 @@
 
 
 // Lekcja nr 70 "Deep Dive into Source Code: jQuery - Part 1"
+// Lekcja nr 71 "Deep Dive into Source Code: jQuery - Part 2"
 
 // JQuery pozwala Ci wyszkuwiać elementu tak jak w css
 var q = $("ul.people li");
 console.log(q); // zwróci to jQuery.fn.init(3)
 
+
+// Lekcja nr 72 "Deep Dive into Source Code: jQuery - Part 3"
+
+// Poniżej "Method chaining" z którego można korzystać w jQuery (w czystym JS, raczej ciężko)
+var q = $("ul.people").addClass("newclass").removeCLass("people");
+console.log(q);
+
+// METHOD CHAINING: Calling one method after another, and each method affects the parent object.
+// So obj.method1().method2() where both methods end up with "this" variable pointing at "obj".
